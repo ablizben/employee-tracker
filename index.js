6d7c3363f2b304d2ad1eaa2869e57882624c4481
Abby Benjamin <ablizben@gmail.com>
2020-08-17 14:06:21 -0400

    Adding functionality to add Dept, Role, Employee

--- index.js
+++ index.js
@@ -1,123 +1,233 @@
 //DEPENDENCIES
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-require("console.table");
+const consoleTable = require("console.table");
 
 //CREATING DATABASE CONNECTION
 var connection = mysql.createConnection({
-    host: "localhost",
-  
-    // Your port; if not 3306
-    port: 3306,
-  
-    // Your username
-    user: "root",
-  
-    // Your password
-    password: "Quito18!",
-    database: "employer_trackerDB"
-  });
-  
-connection.connect(function(err) {
-    if (err) throw err;
-    mainMenu();
-  });
-  
-  function mainMenu() {
-    inquirer
-      .prompt({
-        name: "mainMenu",
-        type: "list",
-        message: "What would you like to do?",
-        choices: [
-         "View All Employees",
-         "View All Employees By Department",
-         "View All Employees By Manager",
-         "Add Employee",
-         "Update Employee Role",
-         "View All Roles",
-         "Exit"
-        ]
-      })
-      .then(function(answer) {
-        switch (answer.mainMenu) {
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "Quito18!",
+  database: "employer_trackerDB"
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  mainMenu();
+});
+
+function mainMenu() {
+  inquirer
+    .prompt({
+      type: "list",
+      name: "mainMenu",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "View All Departments",
+        "View All Roles",
+        "Add Employee",
+        "Add Department",
+        "Add Role",
+        "Update Employee Role",
+        "Exit"
+      ]
+    })
+    .then(function (answer) {
+      switch (answer.mainMenu) {
         case "View All Employees":
           viewAllEmployees();
           break;
-  
-        case "View All Employees By Department":
-          employeesByDept();
+
+        case "View All Departments":
+          viewAllDept();
           break;
-  
-        case "View All Employees By Manager":
-          employeesByMan();
+
+        case "View All Roles":
+          viewAllRoles();
           break;
-  
+
         case "Add Employee":
           addEmployee();
           break;
-  
+
+        case "Add Department":
+          addDept();
+          break;
+
+        case "Add Role":
+          addRole();
+          break;
+
         case "Update Employee Role":
           updateEmployeeRole();
           break;
 
-        case "View All Roles":
-            viewAllRoles();
-            break;
-
         case "Exit":
-          connection.end;
+          connection.end();
           break;
-        }
-      });
-  }
-  
-  function viewAllEmployees() {
-      var query = ""
-      connection.query(query, function(err, res) {
-        console.table(res);
-          mainMenu();
-        });
       }
-      
-    function employeesByDept() {
-    var query = "SELECT * FROM department"
-    connection.query(query, function(err, res) {
+    });
+}
+
+function viewAllEmployees() {
+  var query = "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.department_name AS department, role.salary FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department on role.department_id = department.id"
+  connection.query(query, function (err, res) {
+    console.table(res);
+    mainMenu();
+  });
+}
+
+function viewAllDept() {
+  var query = "SELECT * FROM department"
+  connection.query(query, function (err, res) {
+    console.table(res);
+    mainMenu();
+  });
+}
+
+function viewAllRoles() {
+  var query = "SELECT * FROM role"
+  connection.query(query, function (err, res) {
+    console.table(res);
+    mainMenu();
+  });
+}
+
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter the employee's first name",
+        name: "firstName"
+      },
+      {
+        type: "input",
+        message: "Enter the employee's last name",
+        name: "lastName"
+      },
+      {
+        type: "input",
+        message: "Enter the employee's role ID",
+        name: "addEmployRole"
+      },
+      {
+        type: "input",
+        message: "Enter the employee's manager ID",
+        name: "addEmployMan"
+      }
+    ])
+    .then(function (res) {
+      const firstName = res.firstName;
+      const lastName = res.lastName;
+      const employRoleID = res.addEmployRole;
+      const employManID = res.addEmployMan;
+      const query = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${firstName}", "${lastName}", "${employRoleID}", "${employManID}")`;
+      connection.query(query, function (err, res) {
+        if (err) {
+          throw err;
+        }
         console.table(res);
         mainMenu();
-        });
+      });
+    });
+}
+
+function addDept() {
+  inquirer
+    .prompt({
+      type: "input",
+      message: "Enter the name of the new department",
+      name: "newDept"
+    })
+    .then(function (res) {
+      const newDepartment = res.newDept;
+      const query = `INSERT INTO department (department_name) VALUES ("${newDepartment}")`;
+      connection.query(query, function (err, res) {
+        if (err) {
+          throw err;
+        }
+        console.table(res);
+        mainMenu();
+      });
+    });
+}
+
+function addRole() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter the employee's title",
+        name: "roleTitle"
+      },
+      {
+        type: "input",
+        message: "Enter the employee's salary",
+        name: "roleSalary"
+      },
+      {
+        type: "input",
+        message: "Enter the employee's department ID",
+        name: "roleDept"
+      }
+    ])
+    .then(function (res) {
+      const title = res.roleTitle;
+      const salary = res.roleSalary;
+      const departmentID = res.roleDept;
+      const query = `INSERT INTO role (title, salary, department_id) VALUES ("${title}", "${salary}", "${departmentID}")`;
+      connection.query(query, function (err, res) {
+        if (err) {
+          throw err;
+        }
+        console.table(res);
+        mainMenu();
+      });
+    });
+}
+
+function updateEmployeeRole() {
+  // const query = "SELECT id, first_name, last_name, role_id FROM employee";
+  // connection.query(query, function(err, res) {
+  //   if (err)
+  //     throw err;
+  //     console.table(res);
+    inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter the employee's ID you want to be updated",
+        name: "updateEmploy"
+      },
+      {
+        type: "input",
+        message: "Enter the new role ID for that employee",
+        name: "newRole"
+      }
+    ])
+    .then(function (res) {
+        const updateEmploy = res.updateEmploy;
+        const newRole = res.newRole;
+        const queryUpdate = `UPDATE employee SET role_id = "${newRole}" WHERE first_name = "${updateEmploy}")`;
+        connection.query(queryUpdate, function (err, res) {
+          if (err) {
+            throw err;
+          }
+          console.table(res);
+          mainMenu();
+        })
+      });
     }
+  
 
-    function employeesByMan() {
-        var query = ""
-        connection.query(query, function(err, res) {
-          console.table(res);
-            mainMenu();
-            });
-        }
-    
-    function addEmployee() {
-        var query = ""
-        connection.query(query, function(err, res) {
-          console.table(res);
-            mainMenu();
-            });
-        }
 
-    function updateEmployeeRole() {
-        var query = ""
-        connection.query(query, function(err, res) {
-          console.table(res);
-            mainMenu();
-            });
-        }
-    
-    function viewAllRoles() {
-        var query = "SELECT * FROM role"
-        connection.query(query, function(err, res) {
-          console.table(res);
-            mainMenu();
-            });
-        }
-            
-        
+
