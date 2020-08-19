2aa904f5e84581eb843a5c4852e9a44fdc5b498b
Abigail Benjamin <ablizben@gmail.com>
2020-08-19 15:25:18 -0400

    Separating the values by parentheses

--- seeds.sql
+++ seeds.sql
@@ -1,7 +1,7 @@
 use employer_trackerDB; 
 
 INSERT INTO department (department_name)
-VALUES ("Sales", "Engineering", "Finance", "Legal", "Administration");
+VALUES ("Sales"), ("Engineering"), ("Finance"), ("Legal"), ("Administration");
 
 INSERT INTO role (title, salary, department_id)
 VALUES ("Lead Engineer", 100000, 2), ("Software Engineer", 70000, 2), 
